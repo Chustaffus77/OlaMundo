cf0d11fc65ecf066548acb1f9bd55e29ee887749
Gustavo Baptista <chustaffus777@gmail.com>
2023-08-01 09:59:44 -0300

    Exércios envolvendo variáveis no Python.

---
+++ exercicio01.py
@@ -0,0 +1,29 @@
+#ELABORE UM PROGRAMA QUE PERGUNTE AO ALUNO SUAS 3 NOTAS ESCOLARES.
+#O programa deverá calcular a média das 3 notas inseridas e exibir esta média.
+
+
+
+
+nome = input("Qual é o seu nome?")
+
+nota1 = float(input("Por favor, insira uma nota escolar sua:"))
+
+nota2 = float(input("Insira mais outra agora:"))
+
+nota3 = float(input("Para finalizar insira mais uma:"))
+
+
+media = (nota1 + nota2 + nota3)/3
+
+print("A sua média escolar para essas notas é:",media)
+print("Espero ter ajudado,",nome,"!Volte Sempre!")
+
+
+
+
+
+#Etapa 1: Entrada de dados
+
+#Etapa 2: Processamento de Dados
+
+#Etapa 3: Saida de Dados (resposta na tela de Usuário)
---
+++ exercicio02.py
@@ -0,0 +1,15 @@
+#Fazer um programa que pergunte um número inteiro e apresente ao usuário o antecessor e o sucessor do número informado.
+
+nome = input("Qual é o seu nome?")
+
+num = int(input("Insira um número inteiro:"))
+
+antecessor = num - 1
+
+sucessor = num + 1
+
+print("O número",num, "tem como antecessor o nº",antecessor)
+print("Já o sucessor dele é o nº",sucessor)
+
+
+print("Espero ter ajudado,",nome,"!Volte Sempre!")

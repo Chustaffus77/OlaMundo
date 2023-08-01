f6d29fd8935e41f23103aa86b6acc05f85aafe5b
Gustavo Baptista <chustaffus777@gmail.com>
2023-08-01 09:20:18 -0300

    Teste envolvendo os tipos de variáveis!

--- variavel.py
+++ variavel.py
@@ -1,7 +1,109 @@
-nome = input("Qual é o seu nome?") #yoooooooooooooo PRIMEIRO COMENTÁRIOOOOOOOO#
+#nome = input("Qual é o seu nome?") #yoooooooooooooo PRIMEIRO COMENTÁRIOOOOOOOO#
 
 
-print("Olá,",nome,".Tudo bem com você?")
-print("Alteração depois de clonado")
+#print("Olá,",nome,".Tudo bem com você?")
+
+#print( type(nome) )
+
+a = 10
+
+b = 5.8
+
+c = "Rio de Janeiro"
+
+d = True
+
+
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("d:",d)
+
+print("Tipo de var a: ", type(a))
+print("Tipo de var b: ", type(b))
+print("Tipo de var c: ", type(c))
+print("Tipo de var d: ", type(d))
+
+
+a = 330
+print("a:",a)
+
+b = "São Paulo"
+print("b:",b)
+
+print("Tipo de var a: ", type(a))
+print("Tipo de var b: ", type(b))
+
+
+a = input("Informe o número: ")
+
+b = input("Agora,informe outro número: ")
+
+
+print("a:",a, "- b:",b)
+print("Tipo de var a: ", type(a))
+print("Tipo de var b: ", type(b))
+c = a + b #concatenou as strings de A e B
+
+print("c:",c)
+print("Tipo de var c:", type(c))
+
+d = int(a)
+print("d:", d)
+print("Tipo de var d: ", type(d))
+
+d = int(a)
+print("d:", d)
+print("Tipo de var d: ", type(d))
+
+
+a = int (input("Informe o número: "))
+
+b = int (input("Agora,informe outro número: "))
+
+
+print("a:",a, "- b:",b)
+print("Tipo de var a: ", type(a))
+print("Tipo de var b: ", type(b))
+
+c = a + b
+
+print("c:",c)
+print("Tipo de var c:", type(c))
+
+
+
+
+a = bool(input("Informe o número: "))
+
+b = bool(input("Agora,informe outro número: "))
+
+
+print("a:",a, "- b:",b)
+print("Tipo de var a: ", type(a))
+print("Tipo de var b: ", type(b))
+
+c = a + b
+
+print("c:",c)
+print("Tipo de var c:", type(c))
+
+
+
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------------#
+                                                    #A N O TA Ç Õ E S#
+
+
+#STR= String => Alfanumérico- Letras e Números.
+#INT= Inteiro => Números Inteiros.
+#Float= Reais. => Números Reais.
+#Bool= Boolean. => Aceita apenas "True" ou "False".
+
+
+
 
 
